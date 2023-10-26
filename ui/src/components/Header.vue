55277bd025b3a237997674dcb95fda1c60576baa
Alan Graham <alan@nettica.com>
2023-10-26 14:03:03 +0000

    lighthouse feedback

--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -1,25 +1,25 @@
 <template>
     <div>
         <v-app-bar app clipped-left class="hidden-xs-only">
-            <a href="https://nettica.com"><img class="mr-3" src="/logo.png" height="50" width="50" /></a>
+            <a href="https://nettica.com"><img class="mr-3" src="/logo.png" height="50" width="50" alt="nettica" /></a>
             <v-toolbar-title to="/">
                 {{ title }}</v-toolbar-title>
 
             <v-spacer />
             <v-toolbar-items>
-                <v-btn to="/services" v-show="showServices">
+                <v-btn to="/services" v-show="showServices" title="services" >
                     Services
                     <v-icon right dark>mdi-weather-cloudy</v-icon>
                 </v-btn>
-                <v-btn to="/networks" right>
+                <v-btn to="/networks" right title="networks" >
                     Networks
                     <span class="material-symbols-outlined">hub</span>
                 </v-btn>
-                <v-btn to="/devices">
+                <v-btn to="/devices" title="devices">
                     Devices
                     <v-icon right dark>mdi-devices</v-icon>
                 </v-btn>
-                <v-btn to="/accounts">
+                <v-btn to="/accounts" title="account">
                     Account
                     <v-icon right dark>mdi-account-group</v-icon>
                 </v-btn>
@@ -29,7 +29,7 @@
                 <template v-slot:activator="{ on }">
                     <v-btn icon v-on="on">
                         <v-avatar size="36">
-                            <img :src="picture" height="36" width="36" />
+                            <img :src="picture" height="36" width="36" :alt="name" />
                         </v-avatar>
                     </v-btn>
                 </template>
@@ -39,7 +39,7 @@
                             <div class="overline mb-4">connected as</div>
                             <v-list-item-title class="headline mb-1">{{ name }}
                                 <v-avatar size="64">
-                                    <img alt="name" :src="picture" height="64" width="64" />
+                                    <img alt="name" :src="picture" height="64" width="64" :alt="name" />
                                 </v-avatar>
                             </v-list-item-title>
                             <v-list-item-subtitle>Email: {{ email }}</v-list-item-subtitle>
@@ -57,7 +57,7 @@
             </v-menu>
         </v-app-bar>
         <v-app-bar app clipped-left class="hidden-sm-and-up">
-            <a href="https://nettica.com"><img class="mr-3" src="/logo.png" height="50" width="50" /></a>
+            <a href="https://nettica.com"><img class="mr-3" src="/logo.png" height="50" width="50" alt="nettica"/></a>
             <v-toolbar-title to="/">
                 {{ title }}</v-toolbar-title>
 
