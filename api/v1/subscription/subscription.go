46acc19b3874e371c7f8d760163c7a76a5f223bb
Alan Graham <alan@nettica.com>
2024-03-31 02:16:34 +0000

    HelioPay M1 checkpoint

--- api/v1/subscription/subscription.go
+++ api/v1/subscription/subscription.go
@@ -24,12 +24,302 @@ func ApplyRoutes(r *gin.RouterGroup) {
 	g := r.Group("/subscriptions")
 	{
 
+		g.POST("/helio", createHelioSubscription)
 		g.POST("", createSubscription)
 		g.GET("/:id", readSubscription)
 		g.PATCH("/:id", updateSubscription)
 		g.DELETE("/:id", deleteSubscription)
 		g.GET("", readSubscriptions)
 	}
+}
+
+func createHelioSubscription(c *gin.Context) {
+	var body string
+	var sub map[string]interface{}
+
+	HelioPaylinkIdRelay := os.Getenv("HELIO_PAYLINK_ID_RELAY")
+	HelioPaylinkIdPremium := os.Getenv("HELIO_PAYLINK_ID_PREMIUM")
+	HelioPaylinkIdPro := os.Getenv("HELIO_PAYLINK_ID_PRO")
+
+	HelioPaylinkTokenRelay := os.Getenv("HELIO_PAYLINK_TOKEN_RELAY")
+	HelioPaylinkTokenPremium := os.Getenv("HELIO_PAYLINK_TOKEN_PREMIUM")
+	HelioPaylinkTokenPro := os.Getenv("HELIO_PAYLINK_TOKEN_PRO")
+
+	// read and log the request body
+
+	bytes, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Error("failed to read request body")
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		return
+	}
+
+	body = string(bytes)
+	// remove all the backslashes from the body (is this needed?)
+	body = strings.Replace(body, "\\", "", -1)
+	log.Info(body)
+	bytes = []byte(body)
+
+	// unmarshal the body into a map
+	err = json.Unmarshal(bytes, &sub)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Error("failed to unmarshal request body")
+		// c.AbortWithStatus(http.StatusUnprocessableEntity)
+		// return with no error so webhook doesn't get disabled
+		c.JSON(http.StatusOK, body)
+		return
+	}
+
+	log.Info(sub)
+
+	var sku string
+	var sharedKey string
+
+	// get the transact id from the sub object
+
+	transact := sub["transaction"].(map[string]interface{})
+
+	paylinkId := transact["paystream"].(string)
+
+	switch paylinkId {
+	case HelioPaylinkIdRelay:
+		sku = "Relay-1"
+		sharedKey = "Bearer " + HelioPaylinkTokenRelay
+	case HelioPaylinkIdPremium:
+		sku = "Premium-5"
+		sharedKey = "Bearer " + HelioPaylinkTokenPremium
+	case HelioPaylinkIdPro:
+		sku = "Pro-10"
+		sharedKey = "Bearer " + HelioPaylinkTokenPro
+	default:
+		log.Errorf("unknown paylink_id %s", paylinkId)
+	}
+
+	// Read the Authorization header
+	authorization := c.Request.Header.Get("Authorization")
+	if authorization == "" {
+		log.Error("Authorization header is empty")
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is empty"})
+		return
+	}
+
+	// Compare the sharedKey with the Authorization header
+
+	if sharedKey != authorization {
+		log.Errorf("Authenication denied")
+		// c.JSON(http.StatusUnauthorized, gin.H{"error": "Authenication denied"})
+		// return
+	}
+
+	// walk the json and find the email address
+
+	meta := transact["meta"].(map[string]interface{})
+	log.Info(meta)
+
+	customer := meta["customerDetails"].(map[string]interface{})
+	log.Info(customer)
+
+	email := customer["email"].(string)
+	log.Info(email)
+
+	transStatus := sub["event"].(string)
+
+	var status string
+
+	switch transStatus {
+	case "STARTED":
+		status = "Active"
+	case "STOPPED":
+		status = "Expired"
+	case "CANCELLED":
+		status = "Cancelled"
+	default:
+		log.Errorf("unknown transaction status %s", transStatus)
+	}
+
+	go func() {
+
+		customer_name := "Me"
+
+		// generate a random subscription id
+		id, err := util.RandomString(8)
+		if err != nil {
+			log.Error(err)
+		}
+
+		credits := 0
+		name := ""
+		description := ""
+		devices := 0
+		networks := 0
+		members := 0
+		relays := 0
+
+		// set the credits, name, and description based on the sku
+		switch sku {
+		case "Starter-0":
+			credits = 0
+			name = "Starter"
+			description = "The Starter subscription"
+			devices = core.GetDefaultMaxDevices()
+			networks = core.GetDefaultMaxNetworks()
+			members = core.GetDefaultMaxMembers()
+			relays = core.GetDefaultMaxServices()
+
+		case "Relay-1":
+			fallthrough
+		case "RelayYear-1":
+			credits = 1
+			name = "Relay Service"
+			description = "A single tunnel or relay in any Region"
+			devices = 5
+			networks = 1
+			members = 2
+			relays = 1
+		case "Premium-5":
+			fallthrough
+		case "PremiumYear-5":
+			credits = 5
+			name = "Premium"
+			description = "Up to 5 tunnels or relays in any Region"
+			devices = 25
+			networks = 10
+			members = 5
+			relays = 5
+		case "Pro-10":
+			fallthrough
+		case "ProYear-10":
+			credits = 10
+			name = "Professional"
+			description = "Up to 10 tunnels or relays in any Region"
+			devices = 100
+			networks = 25
+			members = 25
+			relays = 10
+		default:
+			log.Errorf("unknown sku %s", sku)
+		}
+
+		// set the limits based on the sku
+		accounts, err := core.ReadAllAccounts(email)
+		if err != nil {
+			log.Error(err)
+		} else {
+			//  If there's no error and no account, create one.
+			if len(accounts) == 0 {
+				var account model.Account
+				account.Name = customer_name
+				account.AccountName = "Company"
+				account.Email = email
+				account.Role = "Owner"
+				account.Status = "Active"
+				account.CreatedBy = email
+				account.UpdatedBy = email
+				account.Picture = "/account-circle.svg"
+
+				a, err := core.CreateAccount(&account)
+				log.Infof("CREATE ACCOUNT = %v", a)
+				if err != nil {
+					log.Error(err)
+				}
+				accounts, err = core.ReadAllAccounts(email)
+				if err != nil {
+					log.Error(err)
+				}
+
+			}
+		}
+
+		var account *model.Account
+		for i := 0; i < len(accounts); i++ {
+			if accounts[i].Id == accounts[i].Parent {
+				account = accounts[i]
+				break
+			}
+		}
+
+		if account == nil {
+			log.Errorf("account not found for email %s", email)
+			return
+		}
+
+		limits, err := core.ReadLimits(account.Id)
+		if err != nil {
+			log.Error(err)
+			limits_id, err := util.GenerateRandomString(8)
+			if err != nil {
+				log.Error(err)
+			}
+			limits_id = "limits-" + limits_id
+
+			limits = &model.Limits{
+				Id:          limits_id,
+				AccountID:   account.Id,
+				MaxDevices:  0,
+				MaxNetworks: 0,
+				MaxMembers:  0,
+				MaxServices: 0,
+				Tolerance:   core.GetDefaultTolerance(),
+				CreatedBy:   email,
+				UpdatedBy:   email,
+				Created:     time.Now(),
+				Updated:     time.Now(),
+			}
+		}
+
+		limits.MaxDevices += devices
+		limits.MaxNetworks += networks
+		limits.MaxMembers += members
+		limits.MaxServices += relays
+
+		errs := limits.IsValid()
+		if len(errs) != 0 {
+			for _, err := range errs {
+				log.WithFields(log.Fields{
+					"err": err,
+				}).Error("limits validation error")
+			}
+			return
+		}
+
+		// save limits to mongodb
+		mongo.Serialize(limits.Id, "id", "limits", limits)
+
+		// construct a subscription object
+		subscription := model.Subscription{
+			Id:          id,
+			AccountID:   account.Id,
+			Email:       email,
+			Name:        name,
+			Description: description,
+			Issued:      time.Now(),
+			LastUpdated: time.Now(),
+			Expires:     time.Now().AddDate(0, int(quantity), 0),
+			Credits:     credits,
+			Sku:         sku,
+			Status:      status,
+		}
+
+		errs = subscription.IsValid()
+		if len(errs) != 0 {
+			for _, err := range errs {
+				log.WithFields(log.Fields{
+					"err": err,
+				}).Error("subscription validation error")
+			}
+			return
+		}
+
+		// save subscription to mongodb
+		mongo.Serialize(subscription.Id, "id", "subscriptions", subscription)
+
+	}()
+
+	c.JSON(http.StatusOK, body)
 }
 
 func createSubscription(c *gin.Context) {
